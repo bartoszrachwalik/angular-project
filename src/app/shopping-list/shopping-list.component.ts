05f45f22669ab09b6dbacb9609a110ac91aa8764
Bartosz Rachwalik <rachwalikbartosz@gmail.com>
2018-03-25 21:06:32 +0200

    adds ingredient model and displays it

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {Ingredient} from '../shared/ingedient.model';
 
 @Component({
   selector: 'app-shopping-list',
@@ -6,9 +7,13 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
+  ingredients: Ingredient[] = [
+    new Ingredient('Apples', 5),
+    new Ingredient('Tomatos', 10),
+    new Ingredient('Potatos', 3),
+  ];
 
   constructor() {
-
   }
 
   ngOnInit() {
