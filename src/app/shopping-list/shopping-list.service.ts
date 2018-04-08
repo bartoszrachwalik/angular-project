0cdc2b685a9154529afc5d03aa037192f726ec48
Bartosz Rachwalik <rachwalikbartosz@gmail.com>
2018-04-08 19:09:08 +0200

    simplifying comunnication and displaying updated list

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,3 +1,20 @@
-export class ShoppingListService {
+import {Ingredient} from '../shared/ingedient.model';
+import {EventEmitter} from '@angular/core';
 
+export class ShoppingListService {
+  ingredientsChanged = new EventEmitter<Ingredient[]>();
+  private ingredients: Ingredient[] = [
+    new Ingredient('Apples', 5),
+    new Ingredient('Tomatos', 10),
+    new Ingredient('Potatos', 3),
+  ];
+
+  getIngredients() {
+    return this.ingredients.slice();
+  }
+
+  addIngerdient(ingredient: Ingredient) {
+    this.ingredients.push(ingredient);
+    this.ingredientsChanged.emit(this.ingredients.slice());
+  }
 }
