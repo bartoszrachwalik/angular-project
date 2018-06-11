eb30dc5f8e729d07742bc566d7d3b55cc63286a7
Bartosz Rachwalik <rachwalikbartosz@gmail.com>
2018-06-11 17:09:36 +0200

    saves data on server

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,8 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
+
 import {AppComponent} from './app.component';
 import {ShoppingListComponent} from './shopping-list/shopping-list.component';
 import {HeaderComponent} from './header/header.component';
@@ -13,8 +16,8 @@ import {ShoppingListService} from './shopping-list/shopping-list.service';
 import {AppRoutingModule} from './app-routing.module';
 import {RecipeStartComponent} from './recipes/recipe-start/recipe-start.component';
 import {RecipeEditComponent} from './recipes/recipe-edit/recipe-edit.component';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {RecipeService} from './recipes/recipe.service';
+import {DataStorageService} from './shared/data-storage.service';
 
 @NgModule({
   declarations: [
@@ -34,10 +37,14 @@ import {RecipeService} from './recipes/recipe.service';
     BrowserModule,
     FormsModule,
     ReactiveFormsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [ShoppingListService,
-    RecipeService],
+  providers: [
+    ShoppingListService,
+    RecipeService,
+    DataStorageService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,6 @@
 import {Component, OnInit} from '@angular/core';
+import {DataStorageService} from '../shared/data-storage.service';
+import {HttpResponse} from '@angular/common/http';
 
 @Component({
   selector: 'app-header',
@@ -7,10 +9,14 @@ import {Component, OnInit} from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() {
+  constructor(private dataStorageService: DataStorageService) {
   }
 
   ngOnInit() {
   }
 
+  onSave() {
+    this.dataStorageService.storeRecipes()
+      .subscribe((response: HttpResponse<any>) => console.log(response));
+  }
 }
