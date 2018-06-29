805822054503c7752759d92418d207afc2bf97c2
Bartosz Rachwalik <rachwalikbartosz@gmail.com>
2018-06-29 18:08:11 +0200

    creates core module

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,7 +1,8 @@
 import {Component, OnInit} from '@angular/core';
-import {DataStorageService} from '../shared/data-storage.service';
 import {HttpResponse} from '@angular/common/http';
-import {AuthService} from '../auth/auth.service';
+
+import {DataStorageService} from '../../shared/data-storage.service';
+import {AuthService} from '../../auth/auth.service';
 
 @Component({
   selector: 'app-header',
