0113484a3e86beead2091fcbf3bcde9a0e0c914d
Bartosz Rachwalik <rachwalikbartosz@gmail.com>
2018-11-06 14:54:54 +0100

    adds interceptors

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,38 +1,27 @@
 import {Injectable} from '@angular/core';
-import {HttpClient, HttpHeaders, HttpParams, HttpRequest} from '@angular/common/http';
+import {HttpClient} from '@angular/common/http';
 
 import {RecipeService} from '../recipes/recipe.service';
 import {Recipe} from '../recipes/recipe.model';
 import {map} from 'rxjs/operators';
-import {AuthService} from '../auth/auth.service';
 
 @Injectable()
 export class DataStorageService {
 
   constructor(private http: HttpClient,
-              private recipeService: RecipeService,
-              private authService: AuthService) {
+              private recipeService: RecipeService) {
   }
 
   storeRecipes() {
-    const token = this.authService.getToken();
 
-    // return this.http.put(
-    //   'https://angular-project-16d36.firebaseio.com/recipes.json?auth=' + token,
-    //   this.recipeService.getRecipes()
-    // );
-
-    // creates request from scratch
-    const req = new HttpRequest('PUT', 'https://angular-project-16d36.firebaseio.com/recipes.json',
-      this.recipeService.getRecipes(),
-      {reportProgress: true, params: new HttpParams().set('auth', token)});
-    return this.http.request(req);
+    return this.http.put(
+      'https://angular-project-16d36.firebaseio.com/recipes.json',
+      this.recipeService.getRecipes()
+    );
   }
 
   getRecipes() {
-    const token = this.authService.getToken();
-
-    this.http.get('https://angular-project-16d36.firebaseio.com/recipes.json?auth=' + token)
+    this.http.get('https://angular-project-16d36.firebaseio.com/recipes.json')
       .pipe(map(
         (recipes: Recipe[]) => {
           for (const recipe of recipes) {
