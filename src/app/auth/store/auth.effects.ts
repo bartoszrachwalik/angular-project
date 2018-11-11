e9753aadc351bb3785ac232780eec2ab8575ef0f
Bartosz Rachwalik <rachwalikbartosz@gmail.com>
2018-11-11 13:04:55 +0100

    adds signin effect

--- src/app/auth/store/auth.effects.ts
+++ src/app/auth/store/auth.effects.ts
@@ -1,4 +1,5 @@
 import {Injectable} from '@angular/core';
+import {Router} from '@angular/router';
 import {Actions, Effect} from '@ngrx/effects';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/switchMap';
@@ -34,6 +35,31 @@ export class AuthEffects {
       ];
     });
 
-  constructor(private actions$: Actions) {
+  @Effect()
+  authSignin = this.actions$
+    .ofType(AuthActions.TRY_SIGNIN)
+    .map((action: AuthActions.TrySignup) => {
+      return action.payload;
+    })
+    .switchMap((authData: { username: string, password: string }) => {
+      return fromPromise(firebase.auth().signInWithEmailAndPassword(authData.username, authData.password));
+    })
+    .switchMap(() => {
+      return fromPromise(firebase.auth().currentUser.getIdToken());
+    })
+    .mergeMap((token: string) => {
+      this.router.navigate(['/']);
+      return [
+        {
+          type: AuthActions.SIGNIN
+        },
+        {
+          type: AuthActions.SET_TOKEN,
+          payload: token
+        }
+      ];
+    });
+
+  constructor(private actions$: Actions, private router: Router) {
   }
 }
