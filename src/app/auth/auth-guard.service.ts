75b18ed7b278856f98abf6dc752b2717e964eed9
Bartosz Rachwalik <rachwalikbartosz@gmail.com>
2018-11-11 13:15:12 +0100

    auth guard fix

--- src/app/auth/auth-guard.service.ts
+++ src/app/auth/auth-guard.service.ts
@@ -2,6 +2,7 @@ import {ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot} from '@angular
 import {Injectable} from '@angular/core';
 import {Store} from '@ngrx/store';
 import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/take';
 
 import * as fromApp from '../store/app.reducers';
 import * as fromAuth from './store/auth.reducers';
@@ -12,8 +13,10 @@ export class AuthGuard implements CanActivate {
   }
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
-    return this.store.select('auth').map((authState: fromAuth.State) => {
-      return authState.authenticated;
-    });
+    return this.store.select('auth')
+      .take(1)
+      .map((authState: fromAuth.State) => {
+        return authState.authenticated;
+      });
   }
 }
